3602ee6476338832233181f553f0aeb65e928858
Johannes Wolter <jw@inutil.org>
2009-10-30 17:30:58 +0000

    Added license info to new files.

--- amarokctrl.js
+++ amarokctrl.js
@@ -1,6 +1,5 @@
 /*
  *    Copyright (C) 2009 by Johannes Wolter <jw@inutil.org>
- *                          Ian Monroe <ian@monroe.nu>
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
--- httpserver.js
+++ httpserver.js
@@ -15,6 +15,7 @@
  *    You should have received a copy of the GNU General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 Importer.loadQtBinding("qt.core");
 Importer.loadQtBinding("qt.network");
 Importer.include("util.js");
